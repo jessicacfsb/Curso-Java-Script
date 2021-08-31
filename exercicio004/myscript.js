d6c312d7dd7f99c1d0aaec638846b28b337f90c0
jessicacfsb <jessica.cfsb@gmail.com>
2021-08-31 18:29:25 -0300

    exercicio04

---
+++ exercicio004/myscript.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é seu nome?')
+document.write(`Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras<br>`)
+document.write(`Seu nome em letras maiúsculas é ${nome.toUpperCase()}<br>`)
+document.write(`Seu nome em letras minúsculas é ${nome.toLowerCase()}<br>`)
